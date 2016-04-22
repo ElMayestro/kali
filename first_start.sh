9800799b2f4b227fd10fd92118294558fa28ff1e
ElMayestro <timo.gafner@hotmail.com>
2016-04-22 14:30:33 -0500

    Update first_start.sh

--- first_start.sh
+++ first_start.sh
@@ -35,6 +35,14 @@ chown root:root /usr/sbin/add-apt-repository
 /usr/sbin/add-apt-repository ppa:webupd8team/brackets
 apt-get update
 apt-get install brackets -y
+# 1. Add the Spotify repository signing key to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# 3. Update list of available packages
+sudo apt-get update
+# 4. Install Spotify
+sudo apt-get install spotify-client
 cd /home/g/Pictures/
 mkdir Wallpapers
 wget https://github.com/ElMayestro/kali/blob/master/wallpaper.jpeg
